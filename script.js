bc43e48b2770dc9c1dafafe10f8f2920a1373bec
Di10n <dilonchugh@gmail.com>
2024-06-14 14:24:09 -0700

    consistent image sizing

--- script.js
+++ script.js
@@ -4,22 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
     initialize();
 });
 
-function initialize() {
-    const first = window.location.href.split("=")[1] || Math.floor(Math.random()*data.length);
-    const queue = [first];
-    var queuePosition = 0;
+let queuePosition = 0;
+const first = window.location.href.split("=")[1] || Math.floor(Math.random()*data.length);
+const queue = [first];
 
+function initialize() {
     const forward = document.getElementById("forward");
     const backward = document.getElementById("backward");
-
-    function forwardAction() {
-        queuePosition += 1
-        display(queue, queuePosition);
-    }
-    function backwardAction() {
-        if (queuePosition) {queuePosition += -1}
-        display(queue, queuePosition);
-    }
     
     forward.addEventListener("click", forwardAction);
     backward.addEventListener("click", backwardAction);
@@ -34,7 +25,18 @@ function initialize() {
     }
 
     window.addEventListener("keydown", keyHandler);
+    window.addEventListener("resize", adjustImageHeight);
 
+    display(queue, queuePosition);
+    tentimes();
+}
+
+function forwardAction() {
+    queuePosition += 1
+    display(queue, queuePosition);
+}
+function backwardAction() {
+    if (queuePosition) {queuePosition += -1}
     display(queue, queuePosition);
 }
 
@@ -42,6 +44,7 @@ function display(queue, queuePosition) {
     let item = data[queue[queuePosition]];
     document.getElementById("caption").innerHTML = item[0];
     document.getElementById("image").setAttribute("src", item[1]);
+    tentimes();
     document.getElementById("credit").innerHTML = item[2];
 
     if (queuePosition === 0) {
@@ -49,12 +52,13 @@ function display(queue, queuePosition) {
     } else {
         document.getElementById("backward").style.opacity = 1;
     }
-    if (queuePosition === queue.length - 1) {
-        queue.push(Math.floor(Math.random()*data.length))
+    while (queuePosition >= queue.length - 10) {
+        queue.push(Math.floor(Math.random()*data.length));
     }
-
     window.history.pushState("", "", "?id=" + queue[queuePosition]);
-    preload(queue[queuePosition + 1]);
+    for (let i = 1; i <= 10; i++) {
+        preload(queue[queuePosition + i]);
+    }
 }
 
 function preload(itemNumber) {
@@ -62,3 +66,53 @@ function preload(itemNumber) {
     let preload = new Image();
     preload.src = photo;
 }
+
+function adjustImageHeight() {
+    const image = document.getElementById('image');
+    const stuff = document.getElementById('otherStuff');
+  
+    const stuffHeight = stuff.clientHeight;
+    const availableHeight = window.innerHeight - stuffHeight - 50;
+
+    if (window.innerWidth < 500) {
+        image.style.width = window.innerWidth;
+    } else {
+        image.style.maxHeight = `${availableHeight}px`;
+        if (parseInt(image.clientHeight) < parseInt(image.style.maxHeight) && parseInt(image.clientWidth) < window.innerWidth) {
+            let ratioa = parseInt(image.style.maxHeight) / parseInt(image.clientHeight);
+            let ratiob = window.innerWidth / parseInt(image.clientWidth);
+            let ratio = Math.min(ratioa, ratiob);
+            image.style.height = `${image.clientHeight * ratio}px`;
+        } else if (parseInt(image.clientWidth) == window.innerWidth) {
+            image.style.height = `auto`;
+        }
+    }
+    stuff.style.maxWidth = `${image.clientWidth}px`;
+  }
+
+function tentimes() {
+    let x = 10;
+    let interval = 100;
+    for (let i = 0; i < x; i++) {
+        setTimeout(function () {
+            adjustImageHeight();
+        }, i * interval)
+    }
+}
+
+let touchstartX = 0
+let touchendX = 0
+    
+function checkDirection() {
+  if (touchendX + 50 < touchstartX) forwardAction();
+  if (touchendX > touchstartX + 50) backwardAction();
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  checkDirection();
+})
